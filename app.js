147dd95c2b03f7821e7ca21f5b77ab2d8dbde2ce
takahara tomoaki <jihyun8286@gmail.com>
2022-02-24 05:16:42 +0900

    指数オーダーのfib関数の実装

--- app.js
+++ app.js
@@ -1,7 +1,23 @@
 'use strict';
+// 全部足す
 const number = process.argv[2] || 0;
 let sum = 0;
 for (let i = 1; i <= number; i++) {
   sum = sum + i;
 }
 console.log(sum);
+
+// フィボナッチ数列
+// 再起関数
+function fib(n) {
+  if (n === 0) {
+    return 0;
+  } else if (n === 1) {
+    return 1;
+  }
+  return fib(n - 1) + fib(n - 2);
+}
+const length = 40;
+for (let i = 0; i <= length; i++) {
+  console.log(fib(i));
+}
